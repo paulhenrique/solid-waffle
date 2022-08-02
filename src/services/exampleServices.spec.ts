c5b33ac86f37d33ad4f82fb5b218ee6ba8d74fa8
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2022-08-02 20:13:05 -0300

    Adicionando novos serviços

--- src/services/exampleServices.spec.ts
+++ src/services/exampleServices.spec.ts
@@ -1,4 +1,4 @@
-import { sum } from "./exampleServices";
+import { subtract, sum } from "./exampleServices";
 
 test('Should sum a plus b', () => {
   const sumResult = sum(1, 2)
@@ -7,5 +7,10 @@ test('Should sum a plus b', () => {
 
 test('Should sum a plus b correctly', () => {
   const sumResult = sum(15, 2)
+  expect(sumResult).not.toBe(3);
+});
+
+test('Should subtract a and b correctly', () => {
+  const sumResult = subtract(15, 2)
   expect(sumResult).not.toBe(3);
 });
