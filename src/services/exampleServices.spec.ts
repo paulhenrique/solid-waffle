4d33a68a34bc9b05db686c90f227564615b5890e
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2022-08-02 20:09:28 -0300

    Ajustes nos testes

--- src/services/exampleServices.spec.ts
+++ src/services/exampleServices.spec.ts
@@ -4,3 +4,8 @@ test('Should sum a plus b', () => {
   const sumResult = sum(1, 2)
   expect(sumResult).toBe(3);
 });
+
+test('Should sum a plus b correctly', () => {
+  const sumResult = sum(15, 2)
+  expect(sumResult).not.toBe(3);
+});
