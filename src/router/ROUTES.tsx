c6e991aecdcb2be53458e679b0b7fefaf1033715
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2022-08-09 19:05:17 -0300

    Adapt new routes

--- src/router/ROUTES.tsx
+++ src/router/ROUTES.tsx
@@ -1,10 +1,14 @@
 import Home from "../pages/Home";
+import Pandas from "../pages/Pandas";
+import Profile from "../pages/Profile";
 import Settings from "../pages/Settings";
 import { TemplateTypes } from "../templates";
 
 export const routesPath = {
   home: "/",
   settings: "/settings",
+  profile: "/settings/perfil",
+  pandas: "/settings/pandas",
 };
 
 export const ROUTES = [
@@ -16,6 +20,16 @@ export const ROUTES = [
   {
     path: routesPath.settings,
     component: Settings,
-    template: TemplateTypes.default,
+    template: TemplateTypes.settings,
+  },
+  {
+    path: routesPath.profile,
+    component: Profile,
+    template: TemplateTypes.settings,
+  },
+  {
+    path: routesPath.pandas,
+    component: Pandas,
+    template: TemplateTypes.settings,
   },
 ];
